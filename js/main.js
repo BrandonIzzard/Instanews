7bdb62a0780d4dcfa48cb143d15ccfd641cae867
Brandon Izzard <xbrandoni@gmail.com>
2016-11-24 14:21:25 -0800

    switched to google fonts url, deleted font folder.

--- js/main.js
+++ js/main.js
@@ -1,60 +1,60 @@
 import '../sass/style.scss';
 
-$(document).ready(function(){
+$(document).ready(function () {
 	// console.log('Hello Rose');
 	//
 	$('#loading').hide();
 	//
-$('#articles').on('change', function(event) {
-	event.preventDefault();
-	$('#loading').show();
-	var selected = $('.sections').val();
+	$('#articles').on('change', function (event) {
+		event.preventDefault();
+		$('#loading').show();
+		var selected = $('.sections').val();
 
-	var url = 'https://api.nytimes.com/svc/topstories/v2/' + selected + '.json';
-	url += '?' + $.param({
-		'api-key': '94b6dc5c15134d969761539fe65ec9ad'
+		var url = 'https://api.nytimes.com/svc/topstories/v2/' + selected + '.json';
+		url += '?' + $.param({
+			'api-key': '94b6dc5c15134d969761539fe65ec9ad'
+		});
+
+		$.ajax({
+			url: url,
+			method: 'GET',
+			dataType: 'json'
+		})
+
+		.done(function (data) {
+				$('.nyt-logo').css('height', '120px');
+				$('.insta-header').css('height', '25vh');
+				$('.topArticles').empty();
+
+
+				var filterNews = data.results.filter(function (value) {
+					return value.multimedia.length >= 5
+				});
+				filterNews.splice(12);
+
+
+				$.each(filterNews, function (key, value) {
+
+					if (value.multimedia.length > 0) {
+
+						var picture = value.multimedia[4].url,
+							abstract = value.abstract,
+							articleUrl = value.url,
+
+							article = '<li>' + '<a href=' + articleUrl + '>';
+						article += '<div class="articleBackground" style= "background-image:url(' + picture + ')">';
+						article += '<p class="abstract">' + abstract + '</p></div></a></li>';
+
+						$('.topArticles').append(article)
+					}
+				});
+
+			})
+			.fail(function (err) {
+				throw err;
+			})
+			.always(function () {
+				$('#loading').hide();
+			});
 	});
-
-	$.ajax({
-		url: url,
-		method: 'GET',
-		dataType: 'json'
-	})
-
-	.done(function(data) {
-		$('.nyt-logo').css('height', '120px');
-		$('.insta-header').css('height', '25vh');
-		$('.topArticles').empty();
-		
-
-		var filterNews = data.results.filter(function(value){
-			return value.multimedia.length >=5
-		});
-		filterNews.splice(12);
-
-
-		$.each(filterNews, function(key, value) {
-
-			if (value.multimedia.length > 0) {
-
-				var picture = value.multimedia[4].url,
-				abstract = value.abstract,
-				articleUrl = value.url,
-
-				article = '<li>' + '<a href=' + articleUrl + '>';
-				article += '<div class="articleBackground" style= "background-image:url(' + picture + ')">';
-				article += '<p class="abstract">' + abstract + '</p></div></a></li>';
-
-				$('.topArticles').append(article)
-			}
-		});
-
-	})
-	.fail(function(err) {
-		throw err;	
-	})
-	.always(function(){
-    $('#loading').hide();
-    });
-    });
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,40 @@
 var webpack = require("webpack");
 module.exports = {
-	entry: './js/main.js',
-	output: {
-		filename: './build/js/main.min.js'
-	},
+  entry: './js/main.js',
+  output: {
+    filename: './build/js/main.min.js'
+  },
   module: {
-    loaders: [
-    {
-     test: /\.js$/,
-     loader: 'babel',
-     query: {
-      presets: ['es2015']
+    loaders: [{
+      test: /\.js$/,
+      loader: 'babel',
+      query: {
+        presets: ['es2015']
+      }
+    }, {
+      test: /\.scss$/,
+      loaders: ["style", "css", "sass"]
+    }, {
+      test: /\.css$/,
+      loaders: ["style", "css?"]
+
+    }, {
+      test: /\.(eot|svg|ttf|woff|woff2)$/,
+      loader: 'url=loader'
+    }, {
+      test: /\.(jpe?g|png)$/,
+      loader: 'file'
+    }]
+  },
+  devServer: {
+    // watch:true,
+    inline: true,
+    host: '0.0.0.0',
+    port: '3005',
+    watchOptions: {
+      aggregateTimeout: 300,
+      poll: true
     }
-  }, {
-   test: /\.scss$/,
-   loaders: ["style", "css", "sass?sourceMap"]
- },{
-  test: /\.css$/,
-  loaders: ["style", "css?"]  
+  }
 
-},{
-  test: /\.(eot|svg|ttf|woff|woff2)$/,
-  loader: 'file?name=public/fonts/[name].[ext]'
-}, {
- test: /\.(jpe?g|png)$/,
- loader: 'file'
-}]	
-},
-devServer: {
-   		// watch:true,
-   		inline: true,
-   		host: '0.0.0.0',
-   		port: '3005',
-   		watchOptions: {
-   			aggregateTimeout: 300,
-   			poll: true
-   		}
-   	}
-
-   };
+};
